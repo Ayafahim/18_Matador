eea6b40e752aaf6b6548e41a7c5bd7efdc220c30
Mikail <mikail38@live.dk>
2021-01-13 12:26:52 +0100

    Har tilføjet valg af bil farver i GameController klassen

--- src/main/java/com/company/Controllers/GameController.java
+++ src/main/java/com/company/Controllers/GameController.java
@@ -33,15 +33,41 @@ public class GameController {
     */
 
 
+    public void setUpPlayers() {
 
-    public void setUpPlayers(){
-
-        maxPlayers = gui.getUserSelection("vælg antal spillere","2","3","4","5","6");
+        maxPlayers = gui.getUserSelection("vælg antal spillere", "2", "3", "4", "5", "6");
         player = new Player[Integer.parseInt(maxPlayers)];
-        for(int i = 0; i < player.length; i++){
-            player[i] = new Player(gui, Color.cyan);
+        for (int i = 0; i < player.length; i++) {
+            String colors = gui.getUserSelection("Vælg en farve.", "Gul", "Rød", "Grøn", "Blå", "Hvid", "Sort");
+            Color color = null;
+
+            switch (colors) {
+                case "Gul":
+                    color = Color.YELLOW;
+                    break;
+                case "Rød":
+                    color = Color.RED;
+                    break;
+                case "Grøn":
+                    color = Color.GREEN;
+                    break;
+                case "Blå":
+                    color = Color.BLUE;
+                    break;
+                case "Hvid":
+                    color = Color.WHITE;
+                    break;
+                case "Sort":
+                    color = Color.BLACK;
+                    break;
+                default:
+                    color = Color.CYAN;
+            }
+
+
+            player[i] = new Player(gui, color);
             gui.addPlayer(player[i].getGui_player());
-            gui.getFields()[0].setCar(player[i].getGui_player(),true);
+            gui.getFields()[0].setCar(player[i].getGui_player(), true);
         }
 
     }
