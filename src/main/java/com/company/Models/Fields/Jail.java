1d6b14b52e7b2fb5aaca0480ee892edb04a936fa
Alec <Alexranjitkar@gmail.com>
2021-01-14 20:25:55 +0100

    Ændrede navn på parkings feltet og ændrede farve på tax felterne så de har samme farve, derudover er der blevet lavet en instant variabel i jail klassen som hedder subtext

--- src/main/java/com/company/Models/Fields/Jail.java
+++ src/main/java/com/company/Models/Fields/Jail.java
@@ -4,26 +4,29 @@ import java.awt.*;
 
 public class Jail extends Field {
 
-
-
-
     int jailFee;
+    String subText;
 
 
-    public Jail(String name, Color bgColor, Color fgColor, int jailFee) {
+    public Jail(String name, Color bgColor, Color fgColor, String subText, int jailFee) {
         super(name, bgColor, fgColor);
         this.jailFee = jailFee;
     }
 
-    void setJailFee (int jailFee){
+    public void setJailFee (int jailFee){
         this.jailFee = jailFee;
 
+    }
+    public void setSubText(String subText){
+        this.subText = subText;
     }
 
     public int getJailFee(){
         return jailFee;
     }
-
+    public String getSubText(){
+        return subText;
+    }
 
 }
 
