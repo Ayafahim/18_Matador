23a1de65eb78d4ced9d5deca936e209037797012
Mikail <mikail38@live.dk>
2021-01-16 18:18:46 +0100

    Lille bitte rettelse :-)

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -18,13 +18,14 @@ public class Main {
 import com.company.Controllers.GameController;
 import com.company.Models.Player;
 import com.company.Views.BoardGUI;
+import com.company.helper.ConvertHelper;
 import gui_main.GUI;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        GameController game = new GameController(new GUI(BoardGUI.guiFieldsConvert(BoardGUI.fields)));
+        GameController game = new GameController(new GUI(ConvertHelper.guiFieldConvert(BoardGUI.fields)));
 
         game.game();
 
